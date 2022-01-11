e6d712511000f830aec67ec82858b090e5a8b84c
Aldwinny <aldwin0313@yahoo.com.ph>
2022-01-11 15:00:15 +0800

    Comment cleanup, added an after message

--- script/general.js
+++ script/general.js
@@ -1,20 +1,3 @@
-// $(window).scroll(function () {
-//   if (
-//     $("body").scrollTop() > 40 ||
-//     $(document.documentElement).scrollTop() > 40
-//   ) {
-//     $("#nav").css({
-//       "box-shadow": "0 0 10px #212121",
-//       transition: "0.4s",
-//     });
-//   } else {
-//     $("#nav").css({
-//       "box-shadow": "none",
-//       transition: "0.4s",
-//     });
-//   }
-// });
-
 /**
  * The following applies to the navbar,
  * 1. Fades to transparent when on top of the page
